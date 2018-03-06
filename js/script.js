0369f26a80fec59eb4c58b663a34c9332c81baab
Jerry <m08632000@hotmail.com>
2018-03-05 23:10:04 -0800

    added contact.html and contact.scss

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ $(function() {
         interval: 3000
     });
 });
+
+// profile card 
+
+$(document).ready(function(){
+    $('.section__contact-button.button-contact').click(function(){
+        $('.section__contact--profile').addClass('popup')             
+    });
+     $('.section__contact--close').click(function(){
+        $('.section__contact--profile').removeClass('popup')      
+     });
+});
